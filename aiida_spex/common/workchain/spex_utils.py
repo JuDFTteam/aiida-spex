54e14f0e0aac5aa488e1cf0474b9dcfca18f30c0
Anoop Chandran <a.chandran@fz-juelich.de>
2022-07-26 06:46:22 +0200

    Add copyright head

--- aiida_spex/common/workchain/spex_utils.py
+++ aiida_spex/common/workchain/spex_utils.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+###############################################################################
+# Copyright (c), Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany.         #
+#                All rights reserved.                                         #
+# This file is part of the AiiDA-SPEX package.                               #
+#                                                                             #
+# The code is hosted on GitHub at https://github.com/JuDFTteam/aiida-spex     #
+# For further information on the license, see the LICENSE.txt file            #
+# For further information please visit http://www.flapw.de or                 #
+###############################################################################
 """Utilities for `WorkChain` implementations."""
 from __future__ import absolute_import
 
