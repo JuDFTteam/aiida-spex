dfa0b473a17d6945acc32ab50e9aa8ec3006bb0d
Anoop Chandran <a.chandran@fz-juelich.de>
2022-07-26 06:20:15 +0200

    Copyright header adder to individual files

--- aiida_spex/__init__.py
+++ aiida_spex/__init__.py
@@ -1,3 +1,12 @@
+###############################################################################
+# Copyright (c), Forschungszentrum Jülich GmbH, IAS-1/PGI-1, Germany.         #
+#                All rights reserved.                                         #
+# This file is part of the AiiDA-SPEX package.                               #
+#                                                                             #
+# The code is hosted on GitHub at https://github.com/JuDFTteam/aiida-spex     #
+# For further information on the license, see the LICENSE.txt file            #
+# For further information please visit http://www.flapw.de or                 #
+###############################################################################
 """
 The official AiiDA plugin for SPEX.
 """
